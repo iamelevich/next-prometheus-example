116f3e668ffeb936f771f6d759db7154d07053be
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-07-24 20:41:24 +0200

    feat: add fastify-metrics and /api/metrics endpoint
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest } from "fastify";
 import next from "next";
 import { parse } from "url";
+import metricsPlugin from "fastify-metrics";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const isDev = process.env.NODE_ENV !== "production";
@@ -13,6 +14,38 @@ const server = fastify({
 });
 
 app.prepare().then(async () => {
+  // Setup prometheus metrics plugin
+  await server.register(metricsPlugin, {
+    endpoint: "/api/metrics",
+    defaultMetrics: {
+      enabled: true,
+      labels: {
+        name: "next-prometheus-example",
+        version: "0.1.0",
+      },
+    },
+    routeMetrics: {
+      groupStatusCodes: true,
+      routeBlacklist: ["/api/metrics"],
+      customLabels: {
+        name: "next-prometheus-example",
+        version: "0.1.0",
+      },
+      overrides: {
+        labels: {
+          // This is a custom label for the route name. It will try to use pathname or urls if not provided.
+          getRouteLabel: (request: FastifyRequest) => {
+            if (request.routeConfig.statsId) {
+              return request.routeConfig.statsId;
+            }
+            const parsedUrl = parse(request.url, true);
+            return parsedUrl.pathname ?? request.url;
+          },
+        },
+      },
+    },
+  });
+
   server.all("*", async (request, response) => {
     return handle(request.raw, response.raw, parse(request.url, true));
   });
